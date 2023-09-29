ec935520b68c5008bdc99df624a62912cee25efa
Andrew Aiken <andrew.aiken@machinemetrics.com>
2023-09-29 13:58:22 -0400

    chunk

---
+++ Week 5/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "website" {
+  type        = bool
+  default     = false
+  description = "Enable website"
+}
+
+variable "website_policy" {
+  type        = bool
+  default     = false
+  description = "Enable website policy"
+}
